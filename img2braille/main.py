efe720300d85b3f68fbc29d6cf681ff85dbd1af5
GideonBear <87426140+GideonBear@users.noreply.github.com>
2023-01-16 18:07:14 +0100

    Add basic structure to main.py

--- img2braille/main.py
+++ img2braille/main.py
@@ -1,2 +1,11 @@
+from argparse import ArgumentParser, Namespace
+
+
 def main() -> int:
-    ...
+    args = parse_args()
+    # TODO
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser('img2braille')
+    # TODO
